80f90c8b56de26ec949e4c774deea6add6bfdf64
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-04-26 22:45:04 -0400

    Big fix for login error

--- routes/authentication.routes.js
+++ routes/authentication.routes.js
@@ -1,22 +1,23 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
 const router = new Router();
 
-const bcryptjs = require('bcryptjs');
-const mongoose = require('mongoose');
+const bcryptjs = require("bcryptjs");
+const mongoose = require("mongoose");
 const saltRounds = 10;
-const passport = require('passport');
-const User = require('../models/User.model');
+const passport = require("passport");
+const User = require("../models/User.model");
 
-const routeGuard = require('../configs/route-guard.config');
+const routeGuard = require("../configs/route-guard.config");
 
 // .post() route ==> to process form data
-router.post('/signup', (req, res, next) => {
+router.post("/signup", (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
     res.status(401).json({
-      message: 'All fields are mandatory. Please provide your username, email and password.'
+      message:
+        "All fields are mandatory. Please provide your username, email and password.",
     });
     return;
   }
@@ -25,74 +26,83 @@ router.post('/signup', (req, res, next) => {
   if (!regex.test(password)) {
     res.status(500).json({
       message:
-        'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.'
+        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
     });
     return;
   }
 
   bcryptjs
     .genSalt(saltRounds)
-    .then(salt => bcryptjs.hash(password, salt))
-    .then(hashedPassword => {
+    .then((salt) => bcryptjs.hash(password, salt))
+    .then((hashedPassword) => {
       return User.create({
         username,
         email,
-        passwordHash: hashedPassword
+        passwordHash: hashedPassword,
       })
-        .then(user => {
+        .then((user) => {
           // user.passwordHash = undefined;
           // res.status(200).json({ user });
-          req.login(user, err => {
-            if (err) return res.status(500).json({ message: 'Something went wrong with login!' });
+          req.login(user, (err) => {
+            if (err)
+              return res
+                .status(500)
+                .json({ message: "Something went wrong with login!" });
             user.passwordHash = undefined;
-            res.status(200).json({ message: 'Login successful!', user });
+            res.status(200).json({ message: "Login successful!", user });
           });
         })
-        .catch(err => {
+        .catch((err) => {
           if (err instanceof mongoose.Error.ValidationError) {
             res.status(500).json({ message: err.message });
           } else if (err.code === 11000) {
             res.status(500).json({
-              message: 'Username and email need to be unique. Either username or email is already used.'
+              message:
+                "Username and email need to be unique. Either username or email is already used.",
             });
           } else {
             next(err);
           }
         });
     })
-    .catch(err => next(err));
+    .catch((err) => next(err));
 });
 
-router.post('/login', routeGuard, (req, res, next) => {
-  passport.authenticate('local', (err, user, failureDetails) => {
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, failureDetails) => {
     if (err) {
-      res.status(500).json({ message: 'Something went wrong with database query.' });
+      res
+        .status(500)
+        .json({ message: "Something went wrong with database query." });
     }
 
     if (!user) {
       res.status(401).json(failureDetails);
     }
 
-    req.login(user, err => {
-      if (err) return res.status(500).json({ message: 'Something went wrong with login!' });
+    req.login(user, (err) => {
+      if (err)
+        return res
+          .status(500)
+          .json({ message: "Something went wrong with login!" });
       user.passwordHash = undefined;
-      res.status(200).json({ message: 'Login successful!', user });
+      res.status(200).json({ message: "Login successful!", user });
     });
   })(req, res, next);
 });
 
-router.post('/logout', routeGuard, (req, res, next) => {
+router.post("/logout", routeGuard, (req, res, next) => {
   req.logout();
-  res.status(200).json({ message: 'Logout successful!' });
+  res.status(200).json({ message: "Logout successful!" });
 });
 
-router.get('/isLoggedIn', (req, res) => {
+router.get("/isLoggedIn", (req, res) => {
   if (req.user) {
     req.user.passwordHash = undefined;
     res.status(200).json({ user: req.user });
     return;
   }
-  res.status(401).json({ message: 'You are not logged in!' });
+  res.status(401).json({ message: "You are not logged in!" });
 });
 
 module.exports = router;
